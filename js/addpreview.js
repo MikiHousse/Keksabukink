1f37180dad621ebb989aa1c52ea1f6c6dc76f913
MikiHousse <mikichaoss@gmail.com>
2022-08-26 15:54:07 +0300

    Исправление ошибки Рабочая версия

---
+++ js/addpreview.js
@@ -0,0 +1,26 @@
+// import { mapAdd } from "./data";
+
+// const cardTemplate = document.querySelector('#card').content.querySelector('.card');
+// const mapList = document.querySelector('.map');
+
+// const renderdAdd = ({Title, Price}) => {
+//   const mapPreview = cardTemplate.cloneNode(true);
+
+//   mapPreview.querySelector('.popup__title');
+//   // mapPreview.querySelector('.popup__text--price')
+
+//   return mapPreview;
+// };
+
+// const renderAdds = () => {
+//   let addListFragment = document.createDocumentFragment();
+
+//   photos.forEach((mapAdd) => {
+//     addListFragment.appendChild(renderPhoto(photo));
+//   });
+
+//   mapList.appendChild(addListFragment);
+
+// };
+
+// export { renderAdds };
